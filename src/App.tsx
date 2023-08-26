47f1959a52655718ddf28742eed86df5b05ce1f2
Alben Kalambukadu <albenk234@gmail.com>
2023-08-26 13:01:01 -0400

    Created a simple responsive chakra ui grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 
 function App() {
   
 
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`, // >= 0px
+      lg: `"nav nav" "aside main"` // >= 992px https://chakra-ui.com/docs/styled-system/responsive-styles
+    }}>
+      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <Show above="lg">
+        <GridItem area='aside' bg='gold'>Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid>
   );
 }
 

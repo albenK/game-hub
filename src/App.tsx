ea037c26b027034d9838b0471acbe0caa530feca
Alben Kalambukadu <albenk234@gmail.com>
2023-08-26 13:17:12 -0400

    Added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 
-function App() {
-  
-
+const App = () => {
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`, // >= 0px
       lg: `"nav nav" "aside main"` // >= 992px https://chakra-ui.com/docs/styled-system/responsive-styles
     }}>
-      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <GridItem area='nav'>
+        <NavBar/>
+      </GridItem>
       <Show above="lg">
         <GridItem area='aside' bg='gold'>Aside</GridItem>
       </Show>
       <GridItem area='main' bg='dodgerblue'>Main</GridItem>
     </Grid>
   );
-}
+};
 
-export default App
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import logo from '../assets/logo.webp';
+
+
+const NavBar = () => {
+    console.log(logo);
+    return (
+        <HStack>
+            <Image src={logo} boxSize='60px'/>
+            <Text>Game Hub</Text>
+        </HStack>
+    );
+};
+
+export default NavBar;
